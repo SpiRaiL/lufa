beb8820f711e0ca6745d75c41842e568568f017e
David Cox <david.cox@justtheengineer.com>
2015-12-01 19:10:34 +0100

    extended fuse byte fix

--- Bootloaders/MassStorage/shortcuts/upload_atmel_ice_linux.sh
+++ Bootloaders/MassStorage/shortcuts/upload_atmel_ice_linux.sh
@@ -6,9 +6,10 @@
 #locked system, disallows all reading
 #sudo avrdude -catmelice_isp -b9600 -pm32u4 -Pusb -v -e -U lfuse:w:0xde:m -U hfuse:w:0xd8:m -Ulock:w:0xFF:m -Uflash:w:BootloaderMassStorage.hex -Ulock:w:0xc4:m 
 
+#was this!
+#sudo avrdude -catmelice_isp -b300 -B128 -pm32u4 -Pusb -v -e -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -Ulock:w:0xFF:m 
 
-sudo avrdude -catmelice_isp -b300 -B128 -pm32u4 -Pusb -v -e -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -Ulock:w:0xFF:m 
-#sudo avrdude -catmelice_isp -b300 -B10 -pm32u4 -Pusb -v -e -Ulock:w:0xFF:m -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -U efuse:w:0xC3:m
+sudo avrdude -catmelice_isp -b300 -B10 -pm32u4 -Pusb -v -e -Ulock:w:0xFF:m -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -U efuse:w:0xC3:m
 
 sudo avrdude -catmelice_isp -b9600 -B10 -pm32u4 -Pusb -v -Uflash:w:BootloaderMassStorage.hex -Ulock:w:0xc4:m 
 
