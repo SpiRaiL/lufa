6318c9e50f78970f91c0cd894a909ab5102bb490
David Cox <david.cox@justtheengineer.com>
2016-02-05 17:36:37 +0100

    swapped the order that the fuses are programmed in

--- Bootloaders/MassStorage/shortcuts/upload_atmel_ice_linux.sh
+++ Bootloaders/MassStorage/shortcuts/upload_atmel_ice_linux.sh
@@ -9,7 +9,10 @@
 #was this!
 #sudo avrdude -catmelice_isp -b300 -B128 -pm32u4 -Pusb -v -e -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -Ulock:w:0xFF:m 
 
-sudo avrdude -catmelice_isp -b300 -B10 -pm32u4 -Pusb -v -e -Ulock:w:0xFF:m -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -U efuse:w:0xC3:m
+#sudo avrdude -catmelice_isp -b300 -B10 -pm32u4 -Pusb -v -e -Ulock:w:0xFF:m -U hfuse:w:0xd8:m -U lfuse:w:0xde:m -U efuse:w:0xC3:m
+
+#put Lfuse first
+sudo avrdude -catmelice_isp -b300 -B10 -pm32u4 -Pusb -v -e -Ulock:w:0xFF:m -U lfuse:w:0xde:m -U hfuse:w:0xd8:m -U efuse:w:0xC3:m
 
 sudo avrdude -catmelice_isp -b9600 -B10 -pm32u4 -Pusb -v -Uflash:w:BootloaderMassStorage.hex -Ulock:w:0xc4:m 
 
